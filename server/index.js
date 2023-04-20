18517360decc9592e5cf05365cf28821c928acd1
jbxamora <jbxamora@icloud.com>
2023-04-20 15:53:00 -0700

    updated routes to reflect transactions in seed data. seeded transaction data

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// IMPORTS
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
@@ -5,12 +6,19 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+
+// ROUTES
 import kpiRoutes from "./routes/kpi.js"
 import productRoutes from "./routes/product.js";
-import Product from "./models/Product.js"
-import KPI from "./models/KPI.js";
-import { kpis, products } from "./data/data.js";
+import transactionRoutes from "./routes/transaction.js";
 
+// MODELS
+import KPI from "./models/KPI.js";
+import Product from "./models/Product.js"
+import Transaction from "./models/Transaction.js";
+
+// DATA
+import { kpis, products, transactions } from "./data/data.js";
 
 // CONFIG
 dotenv.config();
@@ -26,6 +34,7 @@ app.use(cors());
 // ROUTES
 app.use("/kpi", kpiRoutes);
 app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 9000;
@@ -44,6 +53,8 @@ mongoose
         // await mongoose.connection.db.dropDatabase();
         // KPI.insertMany(kpis);
         // Product.insertMany(products);
+        // Transaction.insertMany(transactions);
+
         
     })
     .catch((error) => console.log(`${error} did not connect`));
